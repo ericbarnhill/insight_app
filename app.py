cc68a9adee0f2c843f068c7954e27f4e554e1fe7
ericbarnhill <ericbarnhill@protonmail.ch>
2018-06-20 13:19:24 -0700

    added jinja functionality

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import requests
 from flask import Flask, render_template, request
 app = Flask(__name__)
+from get_letters import get_letters
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    errors = []
+    letters = []
     if request.method == "POST":
+        # get url that the user has entered
         try:
-            search_term = request.form['word']
-            print("word was:")
+            word = request.form['word']
             print(word)
-        except: 
-            print("error")
-    return render_template('index.html')
+            letters = get_letters(word)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, letters=letters)
 
 if __name__ == '__main__':
     app.run()
