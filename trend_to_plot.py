6cef469fa6274fc127e8c5aefb39dcf3cb0e7c10
ericbarnhill <ericbarnhill@protonmail.ch>
2018-06-22 09:15:13 -0700

    debugging heroku: reintroducing plot commands

--- trend_to_plot.py
+++ trend_to_plot.py
@@ -20,17 +20,17 @@ def trend_to_plot(trend):
     except:
         print("Trending term not found in term dictionary.")
         return False
-    #plt.style.use('ggplot')
+    plt.style.use('ggplot')
     n_months = len(ts)
     x = np.linspace(1, n_months, n_months)
-    #plt.plot(x, ts)
-    #plt.xlabel('Month')
-    #plt.ylabel('Number of Mentions')
-    #plt.xticks(x)
+    plt.plot(x, ts)
+    plt.xlabel('Month')
+    plt.ylabel('Number of Mentions')
+    plt.xticks(x)
     random_number = random.randrange(100)
     filename = "plot_" + str(random_number) + ".png"
     print("plot filename:", filename)
-    #plt.savefig("static/"+filename, bbox_inches="tight")
-    #plt.clf()
+    plt.savefig("static/"+filename, bbox_inches="tight")
+    plt.clf()
     return filename
 
